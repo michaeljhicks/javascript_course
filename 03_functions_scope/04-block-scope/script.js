4616c9bd4edd697cde9e99d2322dbe30ec20a504
Michael Hicks <hicks.michael04@gmail.com>
2024-02-21 08:09:42 -0700

    Complete: Block Scope

--- 03_functions_scope/04-block-scope/script.js
+++ 03_functions_scope/04-block-scope/script.js
@@ -1,30 +1,45 @@
-// GLOBAL AND FUNCTION SCOPE
-// Global = variables accessable everywhere
-// alert('hello');
-// console.log(window.innerHeight);
+// BLOCK LEVEL SCOPE
+// VAR is not block level scoped, meaning you can access var anywhere outside of the block
+// VAR is function scoped, meaning is stays inside the function
+// VAR (like bar below) will be added to the window object (check window in console)
 
 const x = 100;
-console.log(x, 'in global');
+const foo = 1;
+var bar = 2;
 
-function run() {
-    console.log(window.innerHeight);
-    console.log(x, 'in function');
+if (true) {
+   const y = 50
+   console.log(x + y);
+}
+
+// console.log(y);
+
+for (let i = 0; i <= 10; i++) {
+   console.log(i);   
+};
+
+// console.log(i);
+
+
+// VAR is not restricted to the if block. This is not good because you 
+// want your variables scoped
+
+// DO NOT USE VAR
+if (true) {
+   const a = 500;
+   let b = 600
+   var c = 700
+};
+
+console.log(c);
+
+// VAR is function scoped, but NOT block scoped
+function run(){
+   var d = 100;
+   console.log(d);
 }
 
 run();
 
- if(true) {
-    console.log(x, 'in block');
- }
-
- // y is function scoped
- function add() {
-    // VARIABLE SHADOWING - the x below overrides the x on line 5
-    const x = 1;
-    const y = 50;
-    console.log(y + x);
- }
-
- add();
-
- // LOCAL SCOPE simply refers to whatever scope  you're in 
+// d will throw an error
+// console.log(d);
