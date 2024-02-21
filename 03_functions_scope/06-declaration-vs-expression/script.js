43a1311b6e45329da74b30c1060f7bb94bdf99ad
Michael Hicks <hicks.michael04@gmail.com>
2024-02-21 08:29:34 -0700

    Complete: Declaration vs Expression

--- 03_functions_scope/06-declaration-vs-expression/script.js
+++ 03_functions_scope/06-declaration-vs-expression/script.js
@@ -1,30 +1,24 @@
-// How scope works with nested functions and nested blocks
-// Nested functions relate to something called CLOSURES
+// Function Declaration vs Expression
 
-function first(){
-   const x = 100;
-
-   function second() {
-      const y = 200
-      console.log(x + y);
-   }
-   // for second() to run, you must call it within first() - below
-   second();
+// FUNCTION DECLARATION - start with the word FUNCTION, then name it...
+// does not need semicolon at end
+// HOISTING allows the console.log on line 10 to be placed above the addDollarSign, and it'll still run
+function addDollarSign(value) {
+   return '$' + value;
 }
-// Call first() here, which will run second();
-first();
+
+console.log(addDollarSign(100));
 
 
+// FUNCTION EXPRESSION - where you create a variable
+// needs semicolon added to end 
+const addPlusSign = function(value) {
+   return '+' + value;
+};
 
-// from the child - which is second() - you can access any variables in the parent - which is first()
-// It doesn't work the other way around
+console.log(addPlusSign(200));
 
-// same rules apply to blocks
-if (true) {
-   const x = 100;
 
-   if (x === 100) {
-      const y = 200;
-      console.log(x+y);
-   }
-}
+// Differences between Function Declaration and Function Express = Hoisting
+// HOISTING = the process of moving all the function and variable declarations to the top of the current scope before the code is run
+// NOTE: only Function Declarations are available before the code is even run 
