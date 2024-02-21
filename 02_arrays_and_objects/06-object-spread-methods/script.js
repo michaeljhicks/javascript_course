496dda38e314eb43a268cbab63da80cb3401a312
Michael Hicks <hicks.michael04@gmail.com>
2024-02-20 19:31:28 -0700

    Complete: Object Spread Literals

--- 02_arrays_and_objects/06-object-spread-methods/script.js
+++ 02_arrays_and_objects/06-object-spread-methods/script.js
@@ -1,47 +1,65 @@
-let x;
+// another way to create an object, by creating an empty one, then adding properties
+// let x;
+// const todo = {};
+// todo.id = 1;
+// x = todo;
+// console.log(x);
 
+// Create Object Literal using new constructor
+let x;
+const todo = new Object();
+todo.id = 1;
+todo.name = 'Buy Milk';
+todo.completed = false;
+x = todo;
+
+// NESTING
 const person = {
-    name: 'John Doe', 
-    age: 30,
-    isAdmin: true,
     address: {
-        street: '123 Main Street',
-        city: 'Boston',
-        state: 'MA',
+        coords: {
+            lat: 42.9384,
+            lng: -71.3232,
+        },
     },
-    hobbies: ['music', 'sports'],
 };
 
-// accessing the attributes of the object literal
-// the dot syntax will be more common
-// when using brackets, it must be in a string format
-x = person.name;
-x = person['age'];
-// access a nested literal 
-x = person.address.state;
-// access a nested array within a literal
-x = person.hobbies[1];
+x = person.address.coords.lat;
 
-// UPDATE PROPERTIES
-person.name = 'Jane Doe';
-person.isAdmin = false;
-x = person;
+const obj1 = {a: 1, b: 2};
+const obj2 = {c: 3, d: 4};
 
-// REMOVE A PROPERTY 
-delete person.age;
-x = person;
+// this puts the two above objects inside of object 3
+// const obj3 = {obj1, obj2};
 
-// ADD A PROPERTY
-person.hasChildren = true;
-x = person;
+// this combines obj1 and obj2 into one object, using the SPREAD operator
+const obj3 = {...obj1, ...obj2};
+// assign is the old school way 
+const obj4 = Object.assign({}, obj1, obj2);
 
-// ADD A FUNCTION 
-person.greet = function () {
-    console.log(`Hello, my name is ${this.name}`)
-}
-// ACCESS THE FUNCTION
-person.greet();
+const todos = [
+    {id: 1, name: 'Buy Milk'},
+    {id: 2, name: 'Pick up kids from school'},
+    {id: 3, name: 'Take out trash'},
+];
 
-x = person;
+x = todos[0].name;
+
+// Object.keys returns the keys into an array
+x = Object.keys(todo);
+
+// Often you'll want to get the length of an Object
+// you want to first get the keys, and then get the length of the array
+x = Object.keys(todo).length;
+
+// Object.values retruns values into an Array
+x = Object.values(todo);
+
+// for when you want to get both keys and values
+// returns an array of the key/value pairs
+x = Object.entries(todo);
+
+// hasOwnProperty returns a boolean. Pass in the key to see if a property is associated
+x = todo.hasOwnProperty('name'); // returns true 
+x = todo.hasOwnProperty('age'); // returns false 
 
 console.log(x);
