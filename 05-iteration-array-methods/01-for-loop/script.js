32c6a44880c06546e181e92fcbfe4aa30ca85205
Michael Hicks <hicks.michael04@gmail.com>
2024-03-04 11:59:35 -0700

    Complete: For Loops

---
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -0,0 +1,31 @@
+/**  FOR LOOP
+ * ITERATION = a process where you repeat somethng over and over again until a certain condition
+ *      is met, or a certain condition is not met
+ * INITIAL EXPRESSION - Initializes a variable/counter
+ * CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met OR until the condition is false
+ * INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
+ * STATEMENT - Code that will be executed each time the loop is run. To execute a 'block' of code, use the '{}' syntax
+ */
+
+// SYNTAX
+// for ([initialExpression]; [conditionalExpression]; [incrementExpression]) {
+//     statement 
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log('Number ' + i);
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     if (i === 7) {
+//         console.log(`${i} is my lucky number`);
+//     } else {
+//         console.log('Number ' + i);
+//     }    
+// }
+
+
+/**  NEST LOOPS */
+for (let i = 0; i <= 10; i++) {
+    console.log('Number ' + i);
+}
