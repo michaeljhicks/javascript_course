516bf63cd27461ca2bfa92ec47a25f6a121979c3
Michael Hicks <hicks.michael04@gmail.com>
2024-03-09 17:06:08 -0700

    Complete: FizzBuzz While Challenge

--- 05-iteration-array-methods/04-fizz-buzz-challenge/script.js
+++ 05-iteration-array-methods/04-fizz-buzz-challenge/script.js
@@ -9,3 +9,20 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// While Loop
+let j = 1;
+
+while (j <= 100) {
+    if (j % 3 === 0 && j % 5 === 0) {
+        console.log('Fizz Buzz');
+    } else if (j % 3 === 0) {
+        console.log('Fizz');
+    } else if (j % 5 === 0) {
+        console.log('Buzz');   
+    } else {
+        console.log(j);
+    }
+
+    j++;
+}
